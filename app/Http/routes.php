e7f738c4899fa496663d237cc70be937bf0b19e4
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-07-18 19:07:20 -0300

    Home is now coming soon

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,13 +5,13 @@ Route::get('/', function()
 	return view('comingsoon');
 });
 
-//Route::group(['middleware' => 'auth'], function()
-//{
-//	Route::group(['namespace' => 'App\Services\Home\Http\Controllers'], function()
-//	{
-//		Route::get('/', ['as' => 'home', 'uses' => 'Home@index']);
-//
-//		Route::get('admin', ['as' => 'admin.home', 'uses' => 'Home@index']);
-//	});
-//});
-//
+Route::group(['middleware' => 'auth', 'prefix' => 'i'], function()
+{
+	Route::group(['namespace' => 'App\Services\Home\Http\Controllers'], function()
+	{
+		Route::get('/', ['as' => 'home', 'uses' => 'Home@index']);
+
+		Route::get('admin', ['as' => 'admin.home', 'uses' => 'Home@index']);
+	});
+});
+
