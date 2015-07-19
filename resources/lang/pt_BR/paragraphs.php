4346c6d39397cc1239214fa749ed900f75e5b78b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-07-19 00:06:20 -0300

    Add language files

---
+++ resources/lang/pt_BR/paragraphs.php
@@ -0,0 +1,11 @@
+<?php
+
+return array (
+    'login-to-your-account' => 'Entre na sua conta',
+    'manage-your-data-safely' => '<strong>Administre </strong> seus dados de forma completa e segura.',
+    'organize-and-read-your-clipping' => '<strong>Organize e Leia</strong> seu clipping, recebendo apenas o que te interessa, e compartilhe com seus colaboradores.',
+    'configure-everything-you-need' => '<strong>Configure</strong> tudo o que for necessário para ter uma experiência de clipping realmente agradável.',
+    'do-not-have-account' => 'Não possui uma conta?',
+    'do-not-remember-password' => 'Não lebra a senha?',
+    'recover-it' => 'Recupere-a',
+);
