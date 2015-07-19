c0018224b8dbc36fc1724d57464a9ef508f47e4e
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-07-19 13:54:56 -0300

    Show notifications

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -3,6 +3,7 @@
 @section('body.contents')
     <div id="sign_in2">
         <div class="container">
+            @include('notifications.flash')
             @yield('container')
         </div>
     </div>
