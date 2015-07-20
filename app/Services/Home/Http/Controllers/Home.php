6f7497b5bc43d3d264720c3d3adad576c111c904
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-07-19 23:47:53 -0300

    Use repository to get paginated result

--- app/Services/Home/Http/Controllers/Home.php
+++ app/Services/Home/Http/Controllers/Home.php
@@ -3,11 +3,14 @@
 namespace App\Services\Home\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use PragmaRX\Sdk\Services\Clipping\Data\Repositories\Clipping as ClippingRepository;
 
 class Home extends Controller
 {
-	public function index()
+	public function index(ClippingRepository $clippingRepository)
 	{
-		return view('home');
+		return
+			view('home')
+				->with('posts', $clippingRepository->paginated());
 	}
 }
