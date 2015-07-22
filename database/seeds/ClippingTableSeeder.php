706f01eb63533f596e766f551877c32c7ac5d6f4
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-07-22 13:54:48 -0300

    Handle exception

--- database/seeds/ClippingTableSeeder.php
+++ database/seeds/ClippingTableSeeder.php
@@ -51,96 +51,15 @@ class ClippingTableSeeder extends Seeder
 		{
 			$parts = explode("\t", $clipping);
 
-			$created_at = Carbon::createFromFormat('m/d/Y H:i:s', $parts[0]);
-			$published_at = Carbon::createFromFormat('m/d/Y', $parts[1]);
-
-			if ($author = $parts[7])
+			try
 			{
-				$author = ClippingAuthor::firstOrCreate(['name' => $author]);
+				$clipping = $this->importClipping($parts);
 			}
-
-			if ($vehicle = $parts[6])
+			catch (Exception $e)
 			{
-				$vehicle = ClippingVehicle::firstOrCreate(['name' => $vehicle]);
-			}
+				echo "error ". $e->getMessage(). "\n";
 
-			if ($category = $parts[6])
-			{
-				$category = ClippingCategory::firstOrCreate(['name' => $category]);
-			}
-
-			if ($locality = $parts[8])
-			{
-				$locality = ClippingLocality::firstOrCreate(['name' => $locality]);
-			}
-
-			$clipping = Clipping::create([
-				'heading' => $parts[2],
-				'subheading' => $parts[3],
-				'body' => $parts[4],
-				'author_id' => $author ? $author->id : null,
-				'vehicle_id' => $vehicle ? $vehicle->id : null,
-				'category_id' => $category ? $category->id : null,
-				'locality_id' => $locality ? $locality->id : null,
-			    'url' => $parts[10],
-			    'article_preview_url' => isset($parts[17]) ? $parts[17] : null,
-				'published_at' => $published_at,
-				'created_at' => $created_at,
-			]);
-
-			foreach ($tags = explode(',', $parts[9]) as $key => $tag)
-			{
-				if ($tag = Tag::findOrCreateTag($tag))
-				{
-					ClippingTag::firstOrCreate([
-						'clipping_id' => $clipping->id,
-						'tag_id' => $tag->id
-					]);
-				}
-			}
-
-			$clippingFile = app()->make(ClippingFile::class);
-
-			if (isset($parts[11]) && ! empty($parts[11]))
-			{
-				$clippingFile->createFor(
-					$clipping,
-					false,
-					false,
-					$parts[11],
-					ClippingFileType::firstorCreate(['name' => 'video'])
-				);
-			}
-
-			$isMainFile = true;
-
-			for ($counter = 11; $counter <= 16; $counter++)
-			{
-				if (isset($parts[$counter]) && ! empty($parts[$counter]))
-				{
-					$file = $clippingFile->createFor(
-						$clipping,
-						$isMainFile,
-						false,
-						$parts[$counter],
-						ClippingFileType::firstorCreate(['name' => 'image'])
-					);
-
-					$isMainFile = $file->file_name_id ? false : $isMainFile;
-				}
-			}
-
-			if (isset($parts[17]) && ! empty($parts[17]))
-			{
-				$file = str_replace('?dl=0', '?dl=1', $parts[17]);
-
-				$clippingFile->createFor(
-					$clipping,
-					false,
-					true, /// this is the snapshot
-					$file,
-					ClippingFileType::firstorCreate(['name' => 'image'])
-				);
+				dd($parts);
 			}
 		}
 	}
@@ -154,13 +73,126 @@ class ClippingTableSeeder extends Seeder
 		return $clippings;
 	}
 
-	private function findCityByName($city)
+	/**
+	 * @param $parts
+	 * @return static
+	 */
+	private function importClipping($parts)
 	{
-		if ( ! $found = City::where('name', '~*', $city)->first())
+		$created_at = Carbon::createFromFormat('m/d/Y H:i:s', $parts[0]);
+		$published_at = Carbon::createFromFormat('m/d/Y', $parts[1]);
+
+		if ($author = $parts[7])
 		{
-			dd($city);
+			$author = ClippingAuthor::firstOrCreate(['name' => $author]);
 		}
 
-		return $found;
+		if ($vehicle = $parts[6])
+		{
+			$vehicle = ClippingVehicle::firstOrCreate(['name' => $vehicle]);
+		}
+
+		if ($category = $parts[6])
+		{
+			$category = ClippingCategory::firstOrCreate(['name' => $category]);
+		}
+
+		if ($locality = $parts[8])
+		{
+			$locality = ClippingLocality::firstOrCreate(['name' => $locality]);
+		}
+
+		$clipping = Clipping::create(
+			[
+				'heading' => $parts[2],
+				'subheading' => $parts[3],
+				'body' => $parts[4],
+				'author_id' => $author
+					? $author->id
+					: null,
+				'vehicle_id' => $vehicle
+					? $vehicle->id
+					: null,
+				'category_id' => $category
+					? $category->id
+					: null,
+				'locality_id' => $locality
+					? $locality->id
+					: null,
+				'url' => $parts[10],
+				'article_preview_url' => isset($parts[17])
+					? $parts[17]
+					: null,
+				'published_at' => $published_at,
+				'created_at' => $created_at,
+			]
+		);
+
+		foreach ($tags = explode(',', $parts[9])
+		         as
+		         $key
+		=>
+		         $tag)
+		{
+			if ($tag = Tag::findOrCreateTag($tag))
+			{
+				ClippingTag::firstOrCreate(
+					[
+						'clipping_id' => $clipping->id,
+						'tag_id' => $tag->id
+					]
+				);
+			}
+		}
+
+		$clippingFile = app()->make(ClippingFile::class);
+
+		if (isset($parts[11]) && !empty($parts[11]))
+		{
+			$clippingFile->createFor(
+				$clipping,
+				false,
+				false,
+				$parts[11],
+				ClippingFileType::firstorCreate(['name' => 'video'])
+			);
+		}
+
+		$isMainFile = true;
+
+		for ($counter = 11;
+		     $counter <= 16;
+		     $counter++)
+		{
+			if (isset($parts[$counter]) && !empty($parts[$counter]))
+			{
+				$file = $clippingFile->createFor(
+					$clipping,
+					$isMainFile,
+					false,
+					$parts[$counter],
+					ClippingFileType::firstorCreate(['name' => 'image'])
+				);
+
+				$isMainFile = $file->file_name_id
+					? false
+					: $isMainFile;
+			}
+		}
+
+		if (isset($parts[17]) && !empty($parts[17]))
+		{
+			$file = str_replace('?dl=0', '?dl=1', $parts[17]);
+
+			$clippingFile->createFor(
+				$clipping,
+				false,
+				true, /// this is the snapshot
+				$file,
+				ClippingFileType::firstorCreate(['name' => 'image'])
+			);
+			return $clipping;
+		}
+		return $clipping;
 	}
 }
