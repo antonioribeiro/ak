92d33d061b175fce446de9902e8e20bb5e61cfe9
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-07-22 22:57:54 -0300

    Send posts to the view

--- app/Services/Admin/Http/Controllers/Admin.php
+++ app/Services/Admin/Http/Controllers/Admin.php
@@ -3,11 +3,24 @@
 namespace App\Services\Admin\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use PragmaRX\Sdk\Services\Clipping\Data\Repositories\Clipping as ClippingRepository;
 
 class Admin extends Controller
 {
+	/**
+	 * @var ClippingRepository
+	 */
+	private $clippingRepository;
+
+	public function __construct(ClippingRepository $clippingRepository)
+	{
+		$this->clippingRepository = $clippingRepository;
+	}
+
 	public function index()
 	{
-		return view('admin.index');
+		return view('admin.index')
+				->with('posts', $this->clippingRepository->paginated());
 	}
 }
+
