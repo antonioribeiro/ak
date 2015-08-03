213bba118e56f7a61f39cc071bb71b23afad5fe5
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-03 12:52:21 -0300

    Move request to SDK

--- app/Services/Admin/Http/Controllers/Clipping.php
+++ app/Services/Admin/Http/Controllers/Clipping.php
@@ -2,9 +2,10 @@
 
 namespace App\Services\Admin\Http\Controllers;
 
+use Flash;
 use Carbon\Carbon;
 use PragmaRX\Sdk\Core\Controller;
-use App\Services\Admin\Http\Requests\ClippingStore;
+use PragmaRX\Sdk\Services\Clipping\Http\Requests\ClippingStore;
 use PragmaRX\Sdk\Services\Clipping\Commands\AddClipping as AddClippingCommand;
 use PragmaRX\Sdk\Services\Clipping\Data\Repositories\Clipping as ClippingRepository;
 
@@ -24,16 +25,16 @@ class Clipping extends Controller
 	{
 		return
 			view('admin.clipping.create')
-				->with('today_date', Carbon::now()->format('m-d-Y'));
+				->with('today_date', Carbon::now()->format('d-m-Y'));
 	}
 
 	public function store(ClippingStore $request)
 	{
 		$this->execute(AddClippingCommand::class, $request->all());
 
-		Flash::message(t('paragraphs.group-deleted'));
+		Flash::message(t('paragraphs.article-was-added'));
 
-		return Redirect::back();
+		return redirect('admin.clipping.create');
 	}
 
 	public function storeValidate(ClippingStore $request)
