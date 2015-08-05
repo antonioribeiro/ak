151cecc34eee06aee644bdbae5b993e1225ac0c1
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-04 22:07:41 -0300

    Correctly redirect

--- app/Services/Admin/Http/Controllers/Clipping.php
+++ app/Services/Admin/Http/Controllers/Clipping.php
@@ -5,9 +5,11 @@ namespace App\Services\Admin\Http\Controllers;
 use Flash;
 use Carbon\Carbon;
 use PragmaRX\Sdk\Core\Controller;
+use PragmaRX\Sdk\Services\Clipping\Data\Entities\ClippingTag;
 use PragmaRX\Sdk\Services\Clipping\Http\Requests\ClippingStore;
 use PragmaRX\Sdk\Services\Clipping\Commands\AddClipping as AddClippingCommand;
 use PragmaRX\Sdk\Services\Clipping\Data\Repositories\Clipping as ClippingRepository;
+use PragmaRX\Sdk\Services\Tags\Data\Entities\Tag;
 
 class Clipping extends Controller
 {
@@ -23,9 +25,12 @@ class Clipping extends Controller
 
 	public function create()
 	{
+		$tags = Tag::orderBy('name')->get()->lists('name');
+
 		return
 			view('admin.clipping.create')
-				->with('today_date', Carbon::now()->format('d-m-Y'));
+				->with('today_date', Carbon::now()->format('d-m-Y'))
+				->with('tags', $tags);
 	}
 
 	public function store(ClippingStore $request)
@@ -34,7 +39,7 @@ class Clipping extends Controller
 
 		Flash::message(t('paragraphs.article-was-added'));
 
-		return redirect('admin.clipping.create');
+		return redirect()->route('admin.clipping.create');
 	}
 
 	public function storeValidate(ClippingStore $request)
